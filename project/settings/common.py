3192b79cdb9f39fdab3be1fdf35562a7d5d3d5aa
Mokhtar Omar <AlMokhtarOmar24@gmail.com>
2025-03-03 00:49:01 +0200

    done

--- project/settings/common.py
+++ project/settings/common.py
@@ -192,13 +192,11 @@ DJOSER = {
     "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS": ["http://localhost:3000"],
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ALL_ORIGINS = True
-CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-
 CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000"
     "http://localhost:3000",
     "http://192.168.1.5:8000",  # أضف أي IP آخر تحتاجه
 
