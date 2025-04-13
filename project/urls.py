d745defd054e6641c6dbd466d02f66289f132fc7
Mokhtar Omar <AlMokhtarOmar24@gmail.com>
2025-04-13 15:29:35 +0200

    media done

--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,8 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls), # admin endpoint 
@@ -35,3 +37,6 @@ urlpatterns = [
     
 
 ] +  debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
